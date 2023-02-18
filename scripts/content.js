abc310fbe0f864a24f467d121237a229f237a535
Sofiane Larbi <sofianef1110@gmail.com>
2023-02-17 22:01:16 -0800

    modify wrapper

--- scripts/content.js
+++ scripts/content.js
@@ -25,9 +25,27 @@ function disableDistraction () {
 chrome.runtime.onMessage.addListener(
     (req, sender, sendResponse) => {
         if (request.peter.enable) {
-            createDistraction();
+            // createDistraction();
+            const main = document.querySelector('body')
+            if (main) {
+                const player = document.createElement('video')
+                player.id = "ay-yo-mama"
+                player.autoplay = true
+                player.muted = true
+                player.style.height = "300px"
+                player.style.width = "100vw"
+                player.style.objectFit = "cover"
+                player.style.position = "sticky"
+                player.style.bottom = "0"
+                player.style.zIndex = "99999999999"
+                const source = document.createElement('source')
+                source.src = "https://tiktokextension.s3.amazonaws.com/peter.mp4"
+                player.appendChild(source)
+                main.appendChild(player)
+            }
         } else if (request.peter.disable) {
-            disableDistraction();
+            // disableDistraction();
+            document.getElementById('ay-yo-mama').remove()
         }
     } 
 )
